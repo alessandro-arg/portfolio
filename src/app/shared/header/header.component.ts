56e3ab7e9d35c9571471ef4a92808d1acad1fd09
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-19 01:10:04 +0200

    feat: the menu buttons have nice hover effects now

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  activeLang: 'en' | 'de' = 'en';
 
+  switchLanguage(lang: 'en' | 'de') {
+    this.activeLang = lang;
+  }
 }
