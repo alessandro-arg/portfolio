0eaa80145425730b9e1afd810520250d72f2321b
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-20 13:18:12 +0200

    working on the title hover animation

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  titleText = 'Frontend';
+  subtitleText = 'DEVELOPER';
+
+  titleLetters: string[] = this.titleText.split('');
+  subtitleLetters: string[] = this.subtitleText.split('');
+
+  onHover(letter: string) {
+    const span = <HTMLElement>event!.target;
+    const currentText = span.innerText;
+
+    if (currentText === currentText.toLowerCase()) {
+      span.innerText = currentText.toUpperCase();
+    } else {
+      span.innerText = currentText.toLowerCase();
+    }
+  }
+
+  onLeave(letter: string) {
+    const span = <HTMLElement>event!.target;
+    const currentText = span.innerText;
+
+    if (currentText === currentText.toLowerCase()) {
+      span.innerText = letter.toLowerCase();
+    } else {
+      span.innerText = letter.toUpperCase();
+    }
+  }
+}
