b333f98149c2d250b6b3b4822c29a89527804574
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-20 14:18:00 +0200

    title hover animation works great

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -11,28 +11,37 @@ export class HeroComponent {
   titleText = 'Frontend';
   subtitleText = 'DEVELOPER';
 
-  titleLetters: string[] = this.titleText.split('');
-  subtitleLetters: string[] = this.subtitleText.split('');
+  titleLetters: { letter: string; originalCase: string }[] = this.titleText
+    .split('')
+    .map((letter) => ({
+      letter,
+      originalCase: letter,
+    }));
 
-  onHover(letter: string) {
-    const span = <HTMLElement>event!.target;
-    const currentText = span.innerText;
+  subtitleLetters: { letter: string; originalCase: string }[] =
+    this.subtitleText.split('').map((letter) => ({
+      letter,
+      originalCase: letter,
+    }));
 
-    if (currentText === currentText.toLowerCase()) {
-      span.innerText = currentText.toUpperCase();
-    } else {
-      span.innerText = currentText.toLowerCase();
+  onHover(
+    letterObj: { letter: string; originalCase: string },
+    event: MouseEvent
+  ) {
+    const letter = event.target as HTMLElement;
+    if (letter) {
+      letter.innerText =
+        letterObj.originalCase === letterObj.originalCase.toUpperCase()
+          ? letterObj.letter.toLowerCase()
+          : letterObj.letter.toUpperCase();
     }
   }
 
-  onLeave(letter: string) {
-    const span = <HTMLElement>event!.target;
-    const currentText = span.innerText;
-
-    if (currentText === currentText.toLowerCase()) {
-      span.innerText = letter.toLowerCase();
-    } else {
-      span.innerText = letter.toUpperCase();
-    }
+  onLeave(
+    letterObj: { letter: string; originalCase: string },
+    event: MouseEvent
+  ) {
+    const letter = event.target as HTMLElement;
+    letter.innerText = letterObj.originalCase;
   }
 }
