378ad8a4c954137be5c0484d1e21c336e5901cd2
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-23 20:28:58 +0200

    working on the skill set section

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -5,8 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './skills-section.component.html',
-  styleUrl: './skills-section.component.scss'
+  styleUrl: './skills-section.component.scss',
 })
 export class SkillsSectionComponent {
+  stage: 'default' | 'transition' | 'revealed' = 'default';
 
+  skillsIcons = [
+    './../../assets/img/skill_set/icons/html.png',
+    './../../assets/img/skill_set/icons/css.png',
+    './../../assets/img/skill_set/icons/js.png',
+    './../../assets/img/skill_set/icons/ts.png',
+    './../../assets/img/skill_set/icons/angular.png',
+    './../../assets/img/skill_set/icons/firebase.png',
+    './../../assets/img/skill_set/icons/git.png',
+    './../../assets/img/skill_set/icons/rest_api.png',
+    './../../assets/img/skill_set/icons/scrum.png',
+    './../../assets/img/skill_set/icons/material_design.png',
+  ];
+
+  onClick() {
+    if (this.stage !== 'default') return;
+    this.stage = 'transition';
+    setTimeout(() => {
+      this.stage = 'revealed';
+    }, 300);
+  }
 }
