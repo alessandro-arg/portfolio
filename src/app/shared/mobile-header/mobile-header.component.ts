7e2a4e4831a68856565bba77ea1ce66815a4be2a
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-30 13:37:53 +0200

    mobile header with navbar is working

--- src/app/shared/mobile-header/mobile-header.component.ts
+++ src/app/shared/mobile-header/mobile-header.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { NavigationService } from '../../navigation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mobile-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mobile-header.component.html',
   styleUrl: './mobile-header.component.scss',
 })
-export class MobileHeaderComponent {}
+export class MobileHeaderComponent {
+  constructor(private navService: NavigationService) {}
+
+  isNavbarOpen = false;
+  hoverLogo = false;
+  activeLang: 'en' | 'de' = 'en';
+
+  @ViewChild('navbar') navbarRef!: ElementRef;
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+  switchLanguage(lang: 'en' | 'de') {
+    this.activeLang = lang;
+  }
+
+  goToSection(section: string) {
+    const el = document.getElementById(section);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+      this.isNavbarOpen = false;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const clickedInsideNavbar = this.navbarRef?.nativeElement.contains(
+      event.target
+    );
+    const clickedNavbarButton = (event.target as HTMLElement).closest(
+      '.navbar_button'
+    );
+
+    if (!clickedInsideNavbar && !clickedNavbarButton) {
+      this.isNavbarOpen = false;
+    }
+  }
+}
