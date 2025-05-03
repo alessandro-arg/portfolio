4ab7e7c851b612367dc53b5faeaff07fb73ae09e
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-03 02:17:39 +0200

    contact section is now responsive

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
@@ -9,4 +9,17 @@ import { FooterComponent } from '../shared/footer/footer.component';
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss',
 })
-export class ContactSectionComponent {}
+export class ContactSectionComponent implements OnInit {
+  isMobile = false;
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.checkScreenSize();
+      window.addEventListener('resize', this.checkScreenSize.bind(this));
+    }
+  }
+
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 970;
+  }
+}
