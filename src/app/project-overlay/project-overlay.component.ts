52b25f8893175641ff26d71f41f8f7e50372ea92
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-06 22:06:28 +0200

    still translating the page

--- src/app/project-overlay/project-overlay.component.ts
+++ src/app/project-overlay/project-overlay.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Project } from '../project-overlay/project.model';
 import { HeaderComponent } from '../shared/header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project-overlay',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TranslateModule],
   templateUrl: './project-overlay.component.html',
   styleUrls: ['./project-overlay.component.scss'],
 })
