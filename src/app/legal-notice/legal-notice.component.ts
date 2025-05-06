30e4ef1a586282047449f43fd9fe38b5b756cf49
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-06 23:44:03 +0200

    everything is translated correctly with the TranslateModule

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
