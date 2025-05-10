9fde524c19a80767e78403a7285ae43694b4d75d
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-10 14:45:33 +0200

    fixed a problem with the translation of the legal-notice, now the TranslateService is declared globally in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslateModule],
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'portfolio';
+  constructor(private translate: TranslateService) {
+    const platformId = inject(PLATFORM_ID);
+
+    if (isPlatformBrowser(platformId)) {
+      const savedLang = localStorage.getItem('selectedLang') as
+        | 'en'
+        | 'de'
+        | null;
+      const langToUse = savedLang || 'en';
+      this.translate.setDefaultLang(langToUse);
+      this.translate.use(langToUse);
+    } else {
+      this.translate.setDefaultLang('en');
+    }
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,15 +14,7 @@ export class HeaderComponent {
   constructor(
     private navService: NavigationService,
     public translate: TranslateService
-  ) {
-    const savedLang = localStorage.getItem('selectedLang') as
-      | 'en'
-      | 'de'
-      | null;
-    const langToUse = savedLang || 'en';
-    this.translate.setDefaultLang(langToUse);
-    this.translate.use(langToUse);
-  }
+  ) {}
 
   get activeLang(): 'en' | 'de' {
     return this.translate.currentLang as 'en' | 'de';
