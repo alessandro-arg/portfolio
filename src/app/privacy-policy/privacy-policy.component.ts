6bc3f959f1e9b66c8a83df7ffd9589da2bbca22f
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-13 13:42:13 +0200

    the privacy policy is working correctly aswell

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderWrapperComponent } from '../shared/header-wrapper/header-wrapper.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    HeaderWrapperComponent,
+    FooterComponent,
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
