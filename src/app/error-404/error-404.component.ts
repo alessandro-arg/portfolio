e7b824a5f8e748e7adb7944baa272c8e366fd487
Alessandro Argenziano <1603alessandro@gmail.com>
2025-07-24 18:07:56 +0200

    added some meta data for the different pages, fixed some bugs

--- src/app/error-404/error-404.component.ts
+++ src/app/error-404/error-404.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-error-404',
@@ -9,4 +10,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './error-404.component.html',
   styleUrl: './error-404.component.scss',
 })
-export class Error404Component {}
+export class Error404Component {
+  constructor(private meta: Meta, private title: Title) {
+    this.title.setTitle('Page Not Found - Alessandro Argenziano');
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        'Oops! That page doesn’t exist. Return home or explore the portfolio of Angular developer Alessandro Argenziano.',
+    });
+  }
+}
